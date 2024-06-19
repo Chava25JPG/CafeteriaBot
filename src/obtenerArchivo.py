f421c4b6830642ef4a945ce98f4f11be2bd75745
Chava25JPG <saseza8@gmail.com>
2024-06-19 12:52:07 -0600

    first commit

--- src/obtenerArchivo.py
+++ src/obtenerArchivo.py
@@ -1,6 +1,6 @@
 import sys
 import json
-from datetime import datetime, timedelta
+from datetime import datetime
 from pytz import timezone
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
@@ -19,34 +19,38 @@ credentials = Credentials.from_service_account_file(
 
 # Construir el servicio de Google Drive y Google Sheets
 drive_service = build('drive', 'v3', credentials=credentials)
-sheets_service = build('sheets', 'v4', credentials=credentials)
 
 def obtener_o_crear_archivo_dia_especifico():
-    """Encuentra o crea un archivo de Google Sheets para el día actual."""
+    """Encuentra o crea un archivo de Google Sheets para el día actual y lo hace público."""
     mx_zone = timezone('America/Mexico_City')
     today = datetime.now(mx_zone).date()
-    # Crear un nombre de archivo que incluya la fecha del día actual
     formatted_file_name = f"Registro de Asistencia {today.strftime('%Y-%m-%d')}"
 
-    # Buscar el archivo por el nombre formateado
     query = f"name='{formatted_file_name}' and mimeType='application/vnd.google-apps.spreadsheet'"
     response = drive_service.files().list(q=query).execute()
     files = response.get('files', [])
 
     if not files:
-        # Si el archivo no existe, crear uno nuevo
-        file_metadata = {
-            'name': formatted_file_name,
-            'mimeType': 'application/vnd.google-apps.spreadsheet'
-        }
-        file = drive_service.files().create(body=file_metadata, fields='id,webViewLink').execute()
-        print(f"Archivo creado: {file.get('webViewLink')}")
-        return file.get('webViewLink')
+        file_metadata = {'name': formatted_file_name, 'mimeType': 'application/vnd.google-apps.spreadsheet'}
+        file = drive_service.files().create(body=file_metadata, fields='id').execute()
+        file_id = file.get('id')
+        print(f"Archivo creado con ID: {file_id}")
     else:
-        # Devolver el enlace del archivo existente
-        file = drive_service.files().get(fileId=files[0]['id'], fields='webViewLink').execute()
-        print(f"Archivo encontrado: {file.get('webViewLink')}")
-        return file.get('webViewLink')
+        file_id = files[0]['id']
+        print(f"Archivo encontrado con ID: {file_id}")
+
+    # Hacer el archivo visible para cualquiera con el enlace
+    permission = {
+        'type': 'anyone',
+        'role': 'reader'
+    }
+    drive_service.permissions().create(fileId=file_id, body=permission).execute()
+
+    # Obtener el enlace webViewLink para compartir
+    file = drive_service.files().get(fileId=file_id, fields='webViewLink').execute()
+    link = file.get('webViewLink')
+    print(f"Enlace para compartir: {link}")
+    return link
 
 if __name__ == '__main__':
     link = obtener_o_crear_archivo_dia_especifico()
