312500bc640ed9c1ec489e06d0df7366279c1e70
Chava25JPG <saseza8@gmail.com>
2024-06-22 02:51:41 -0600

    first commit

--- src/funciones.js
+++ src/funciones.js
@@ -5,62 +5,48 @@ const { spawn } = require('child_process');
 const GOOGLE_DRIVE_FOLDER = '1QqK-zY5dom7WW-fhfAG5TsYkCml05g8B';
 
 async function getAdminsAndStore(chatId) {
-    
+    if (!(await checkBotPermissions(chatId))) {
+        console.log("Bot doesn't have necessary permissions");
+        return;
+    }
+
+    try {
+        const adminData = await getAdmins(chatId);
+        const groupName = await getGroupName(chatId);
+
+        runPythonProcess(groupName, adminData);
+    } catch (error) {
+        console.error("Error in getAdminsAndStore:", error);
+    }
+}
+function runPythonProcess(groupName, adminData) {
+    const pythonProcess = spawn('python3', ['./src/OCRAsis.py', groupName, JSON.stringify(adminData), GOOGLE_DRIVE_FOLDER]);
+
+    pythonProcess.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    pythonProcess.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    pythonProcess.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+}
+async function getAdmins(chatId) {
     try {
         const admins = await bot.getChatAdministrators(chatId);
-        const adminData = admins.map(admin => ({
+        return admins.map(admin => ({
             id: admin.user.id,
             name: `${admin.user.first_name}${admin.user.last_name ? ' ' + admin.user.last_name : ''}`
         }));
-
-        const groupName = await bot.getChat(chatId).then(chat => chat.title);
-
-        const pythonProcess = spawn('python3', ['./src/OCRAsis.py', groupName, JSON.stringify(adminData), GOOGLE_DRIVE_FOLDER]);
-
-        pythonProcess.stdout.on('data', (data) => {
-            console.log(`stdout: ${data}`);
-        });
-
-        pythonProcess.stderr.on('data', (data) => {
-            console.error(`stderr: ${data}`);
-        });
-
-        pythonProcess.on('close', (code) => {
-            console.log(`child process exited with code ${code}`);
-        });
-
-    }  catch (error) {
+    } catch (error) {
         console.error("Error fetching admins:", error);
-        if (error.response) {
-          console.error("Error response:", error.response);
-        }
-        if (error.code) {
-          console.error("Error code:", error.code);
-        }
-      }
+        throw error; // Re-throw the error to be handled by the caller
     }
+}
 
-    async function checkBotPermissions(chatId) {
-        try {
-            const chatMember = await bot.getChatMember(chatId, bot.botInfo.id);
-            console.log("Bot permissions:", chatMember);
-            
-            // Verificar que el bot tenga los permisos necesarios
-            if (chatMember.status !== 'administrator') {
-                console.log("Bot is not an administrator.");
-                return false;
-            }
-            if (!chatMember.can_manage_chat) {
-                console.log("Bot does not have 'can_manage_chat' permission.");
-                return false;
-            }
-    
-            return true;
-        } catch (error) {
-            console.error("Error checking bot permissions:", error);
-            return false;
-        }
-    }
 
 
 module.exports = { getAdminsAndStore };
